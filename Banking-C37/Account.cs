738676d6b669ec9f00ab149c241a489015894d29
Greg Doud <gdoud@gmail.com>
2022-05-04 16:34:15 -0400

    Add project files.

---
+++ Banking-C37/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_C37 {
+    internal class Account {
+
+        private static int NextId = 1;
+
+        public int AcctNbr { get; private set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public decimal Balance { get; private set; } = 0;
+
+        public bool Deposit(decimal Amount) {
+            if(Amount <= 0) {
+                Console.WriteLine("Amount must be > zero");
+                return false;
+            }
+            Balance = Balance + Amount;
+            return true;
+        }
+
+        public bool Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine("Amount must be > zero");
+                return false;
+            }
+            if (Amount > Balance) {
+                Console.WriteLine("Insufficient funds");
+                return false;
+            }
+            Balance = Balance - Amount;
+            return true;
+        }
+
+        public bool Transfer(decimal Amount, Account ToAccount) {
+            var Success = Withdraw(Amount);
+            if (Success) {
+                ToAccount.Deposit(Amount);
+                return true;
+            }
+            return false;
+        }
+
+        public Account() {
+            AcctNbr = NextId++;
+        }
+
+    }
+}
